2086c9e6225d82c546d9faf08b2b41e784b765bb
Alimusharafk <alimusharafpmna@gmail.com>
2023-05-17 13:20:25 +0530

    Initial commit

---
+++ bin/operatorsdart.dart
@@ -0,0 +1,30 @@
+void main(){
+  dynamic a = 100,b = 70;
+  print('a+b = ${a+b}');
+  print('a-b = ${a-b}');
+  print('a*b = ${a*b}');
+  print('a/b = ${a/b}');
+  print('a%b = ${a%b}');
+  print('a~/b = ${a~/b}');
+  print('a*=b  a= ${a=b}');
+  print("_____________");
+  print("a  = b a = ${a=b}");
+  print("a += b a = ${a+=b}");
+  print("a -= b a = ${a-=b}");
+  print("a *= b a = ${a*=b}");
+  print("a /= b a = ${a/=b}");
+  print("a %= b a = ${a%=b}");
+
+  print("________________");
+  int c = 800 , d = 300;
+  print("c > d   -> ${c > d}");
+  print("c < d   -> ${c < d}");
+  print("c >= d   -> ${c >= d}");
+  print("c <= d   -> ${c <= d}");
+  print("c == d   -> ${c == d}");
+  print("c != d   -> ${c != d}");
+
+
+
+
+}
